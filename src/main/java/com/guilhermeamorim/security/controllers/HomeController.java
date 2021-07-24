0ed482bd219aa25c394c18dce5c341a810b5fc8d
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2021-07-24 16:11:55 -0300

    Add security level for users

--- src/main/java/com/guilhermeamorim/security/controllers/HomeController.java
+++ src/main/java/com/guilhermeamorim/security/controllers/HomeController.java
@@ -7,13 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("/login")
-	public String login() {
-		return "login";
-	}
+	public String login() { return "login"; }
 	
 	@RequestMapping("/")
-	public String index() {
-		return "index";
-	}
+	public String index() { return "index"; }
+	
+	@RequestMapping("/admin")
+	public String admin() { return "admin"; }
 
 }
